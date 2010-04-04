be9bc8be996c6179bd892b16ae54514ba2b1d506
Geza Kovacs <gkovacs@mit.edu>
2010-04-04 04:57:53 -0400

    updated website

--- unetbootin-windows-latest.exe/index.php
+++ unetbootin-windows-latest.exe/index.php
@@ -1,3 +1,3 @@
 <?php
-header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-windows-419.exe' ) ;
+header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-windows-429.exe' ) ;
 ?>
