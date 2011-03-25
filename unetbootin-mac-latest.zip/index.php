76696d7007e8884926db8ff1ecbb54b2651e2ef2
Geza Kovacs <gkovacs@mit.edu>
2011-03-25 01:20:37 -0400

    updated website

--- unetbootin-mac-latest.zip/index.php
+++ unetbootin-mac-latest.zip/index.php
@@ -1,3 +1,3 @@
 <?php
-header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-mac-515.zip' ) ;
+header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-mac-515.dmg' ) ;
 ?>
