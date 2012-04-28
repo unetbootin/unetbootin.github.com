3237ceb5da2a25c719a9aaf3d3159e59131d18df
Geza Kovacs <gkovacs@mit.edu>
2012-04-28 04:46:44 -0400

    updated website

--- unetbootin-linux-latest/index.php
+++ unetbootin-linux-latest/index.php
@@ -1,3 +1,3 @@
 <?php
-header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-linux-568' ) ;
+header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-linux-575' ) ;
 ?>
