c289952840cac176f8b69dfe4ba9563a38a3869a
Geza Kovacs <gkovacs@stanford.edu>
2014-06-01 17:24:38 -0700

    updated website

--- unetbootin-linux-latest/index.php
+++ unetbootin-linux-latest/index.php
@@ -1,3 +1,3 @@
 <?php
-header( 'Location: http://downloads.sourceforge.net/unetbootin/unetbootin-linux-605.bin' ) ;
+header( 'Location: http://launchpad.net/unetbootin/trunk/603/+download/unetbootin-linux-603.bin' ) ;
 ?>
