822ffe622d8b2b8767323e02ab685359e2bb0ad8
Geza Kovacs <gkovacs@stanford.edu>
2017-10-24 02:31:07 -0700

    updated website

--- unetbootin-linux-latest/index.php
+++ unetbootin-linux-latest/index.php
@@ -1,3 +1,3 @@
 <?php
-header( 'Location: https://github.com/unetbootin/unetbootin/releases/download/655/unetbootin-linux-655.bin' ) ;
+header( 'Location: https://github.com/unetbootin/unetbootin/releases/download/657/unetbootin-linux-657.bin' ) ;
 ?>
